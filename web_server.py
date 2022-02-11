6ec34b1bbbdd5e02d72f3fef9abffa33f15a1e28
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-02-10 22:34:18 -0500

    respond with titlecut image

--- web_server.py
+++ web_server.py
@@ -1,7 +1,18 @@
+from PIL import Image
+import io
+
+from create_titlecut import *
+
 def app(environ, start_response):
-    data = b"Hello, World!\n"
+
+    image = create_titlecut(target_text='though~ rely on bad ligma!', dictionaries=['azurlane'])
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format='png')
+    image_byte_array = image_bytes.getvalue()
+
+    data = image_byte_array
     start_response("200 OK", [
-        ("Content-Type", "text/plain"),
+        ("Content-Type", "image/png"),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
