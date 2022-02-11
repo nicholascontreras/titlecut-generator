0a436be0afc1bda166434c9a6bc6739ca63b826e
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-02-10 22:14:54 -0500

    init commit

---
+++ create_dicts.py
@@ -0,0 +1,106 @@
+import os
+import json
+
+import PIL
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+def main():
+
+    subject_folders = os.listdir('image_sources')
+    print(subject_folders)
+
+    for cur_subject_folder in subject_folders:
+
+        config_settings = get_config_for_subject(cur_subject_folder)
+        dictionary_data = {'words': {}, 'letters': {}}
+
+        files = os.listdir('image_sources/' + cur_subject_folder)
+        
+        for cur_file in files:
+            if cur_file.lower().endswith(('.png', '.jpg', 'jpeg')):
+                add_image_to_dictionary(dictionary_data, cur_subject_folder, cur_file, config_settings)
+
+        with open('image_sources/' + cur_subject_folder + '/dictionary.json', 'w') as dictionary_file:
+            json.dump(dictionary_data, dictionary_file)
+
+def get_config_for_subject(subject_name: str) -> dict:
+    with open('image_sources/' + subject_name + '/config.json') as config_file:
+        return json.load(config_file)
+
+def add_image_to_dictionary(dictionary_data: dict, subject_name: str, image_name: str, config_settings: dict) -> None:
+    image = Image.open('image_sources/' + subject_name + '/' + image_name)
+    image = preprocess_image(image=image, invert=config_settings['invert'], threshold=config_settings['threshold'])
+
+    words_ocr_results = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+    add_words_to_dictionary(dictionary_data=dictionary_data, ocr_results=words_ocr_results, image_name=image_name, min_confidence=config_settings['word_confidence'])
+
+    letters_ocr_results = pytesseract.image_to_pdf_or_hocr(image, config='-c hocr_char_boxes=1', extension='hocr').decode("utf-8") 
+    add_letters_to_dictionary(dictionary_data=dictionary_data, ocr_results=letters_ocr_results, image_name=image_name, min_confidence=config_settings['letter_confidence'])
+
+
+def add_words_to_dictionary(dictionary_data: dict, ocr_results: dict, image_name: str, min_confidence: float) -> None:
+    num_results = len(ocr_results['level'])
+
+    for i in range(num_results):
+        if (ocr_results['level'][i] == 5):
+            # Only if this result is a word match
+            confidence = float(ocr_results['conf'][i])
+            if confidence > min_confidence:
+                # Greather than 50% confidence
+                text = ocr_results['text'][i]
+                left = int(ocr_results['left'][i])
+                top = int(ocr_results['top'][i])
+                width = int(ocr_results['width'][i])
+                height = int(ocr_results['height'][i])
+
+                if not text in dictionary_data['words']:
+                    dictionary_data['words'][text] = []
+
+                dictionary_data['words'][text].append({'image_name': image_name, 'left': left, 'top': top, 'width': width, 'height': height})
+
+def add_letters_to_dictionary(dictionary_data: dict, ocr_results: str, image_name: str, min_confidence: float) -> None:             
+
+    while True:
+        next_letter_index = ocr_results.find('<span class=\'ocrx_cinfo\'')
+        if next_letter_index == -1:
+            break
+
+        ocr_results = ocr_results[next_letter_index:]
+        ocr_results = ocr_results[ocr_results.index('x_bboxes'):]
+        ocr_results = ocr_results[ocr_results.index(' '):]
+
+        bounding_box_as_strings = ocr_results[:ocr_results.index(';')].strip().split()
+        left = int(bounding_box_as_strings[0])
+        top = int(bounding_box_as_strings[1])
+        right = int(bounding_box_as_strings[2])
+        bottom = int(bounding_box_as_strings[3])
+
+        ocr_results = ocr_results[ocr_results.index('x_conf'):]
+        ocr_results = ocr_results[ocr_results.index(' '):]
+
+        confidence_as_string = ocr_results[:ocr_results.index('\'')].strip()
+        confidence = float(confidence_as_string)
+
+        ocr_results = ocr_results[ocr_results.index('>') + 1:]
+        letter = ocr_results[:ocr_results.index('<')]
+
+        if confidence > min_confidence:
+            if not letter in dictionary_data['letters']:
+                dictionary_data['letters'][letter] = []
+
+            dictionary_data['letters'][letter].append({'image_name': image_name, 'left': left, 'top': top, 'right': right, 'bottom': bottom})
+
+def preprocess_image(image: Image.Image, invert: bool, threshold: int) -> Image.Image:
+    image = image.convert('L')
+
+    if invert:
+        image = PIL.ImageOps.invert(image)
+
+    image = image.point(lambda pixel: 0 if pixel < threshold else 255)
+    return image
+
+if __name__ == '__main__':
+    main()
---
+++ create_titlecut.py
@@ -0,0 +1,105 @@
+import json
+import random
+
+from PIL import Image
+
+def create_titlecut(target_text: str, dictionaries: dict[str]) -> Image.Image:
+
+    combined_dictionary, space_width = load_dictionaries(dictionaries)
+
+    print(space_width)
+
+    output_image_height = 50
+    output_image = Image.new(mode='RGB', size=(1, output_image_height), color='white')
+
+    for cur_word in target_text.split():
+        if cur_word in combined_dictionary['words']:
+            # Use a full word
+            chosen_word_instance = random.choice(combined_dictionary['words'][cur_word])
+            chosen_word_instance_source_image = Image.open('image_sources/'+ chosen_word_instance['subject_name'] + '/' + chosen_word_instance['image_name'])
+
+            # Cut the word out of the source image
+            chosen_word_instance_image = image_subsection(chosen_word_instance_source_image, chosen_word_instance['left'], chosen_word_instance['top'], chosen_word_instance['width'], chosen_word_instance['height'])
+            # Scale the word image to fit our output image
+            chosen_word_instance_image = scale_image_to_height(image=chosen_word_instance_image, height=output_image_height)
+            # Append the word to the output image
+            output_image = append_image(base_image=output_image, image_to_append=chosen_word_instance_image, spacing_after=space_width)
+        else:
+            # Word doesn't exist, use letters to form word!
+            word_length = len(cur_word)
+            cur_letter_index = 0
+            for cur_letter in cur_word:
+                chosen_letter_instance = random.choice(combined_dictionary['letters'][cur_letter])
+                chosen_letter_instance_source_image = Image.open('image_sources/'+ chosen_letter_instance['subject_name'] + '/' + chosen_letter_instance['image_name'])
+
+                chosen_letter_instance_width = chosen_letter_instance['right'] - chosen_letter_instance['left']
+                chosen_letter_instance_height = chosen_letter_instance['bottom'] - chosen_letter_instance['top']
+
+                # Cut the letter out of the source image
+                chosen_letter_instance_image = image_subsection(chosen_letter_instance_source_image, chosen_letter_instance['left'], chosen_letter_instance['top'], chosen_letter_instance_width, chosen_letter_instance_height)
+                # Scale the letter image to fit our output image
+                chosen_letter_instance_image = scale_image_to_height(image=chosen_letter_instance_image, height=output_image_height)
+                # Append the letter to the output image
+                if cur_letter_index == word_length - 1:
+                    output_image = append_image(base_image=output_image, image_to_append=chosen_letter_instance_image, spacing_after=space_width)
+                else:
+                    output_image = append_image(base_image=output_image, image_to_append=chosen_letter_instance_image, spacing_after=1)
+
+                cur_letter_index += 1
+
+    return output_image
+
+def image_subsection(image: Image.Image, left: int, top: int, width: int, height: int, padding_percent: float = 0.1) -> Image.Image:
+    left -= int(width * padding_percent)
+    top -= int(height * padding_percent)
+    right = int(left + width + (width * padding_percent * 2))
+    bottom = int(top + height + (height * padding_percent * 2))
+    return image.crop(box=(left, top, right, bottom))
+
+
+def scale_image_to_height(image: Image.Image, height: int) -> Image.Image:
+    scale_factor = height / image.height
+    scaled_width = int(image.width * scale_factor)
+    scaled_height = int(image.height * scale_factor)
+    return image.resize(size=(scaled_width, scaled_height))
+
+def append_image(base_image: Image.Image, image_to_append: Image.Image, spacing_after: int = 0) -> Image.Image:
+    new_image = Image.new(mode='RGB', size=(base_image.width + image_to_append.width + spacing_after, base_image.height), color='white')
+    new_image.paste(base_image, box=(0, 0))
+    new_image.paste(image_to_append, box=(base_image.width, 0))
+    return new_image
+
+def load_dictionaries(dictionary_names: list[str]) -> tuple[dict, int]:
+
+    combined_dictionary = {'words': {}, 'letters': {}}
+    sum_of_letter_widths = 0
+    num_of_letter_widths = 0
+
+    for cur_dictionary_name in dictionary_names:
+        with open('image_sources/' + cur_dictionary_name + '/dictionary.json') as cur_dictionary:
+            cur_dictionary_data = json.load(cur_dictionary)
+
+            for cur_word in cur_dictionary_data['words']:
+                if not cur_word in combined_dictionary:
+                    combined_dictionary['words'][cur_word] = []
+                
+                for cur_word_instance in cur_dictionary_data['words'][cur_word]:
+                    cur_word_instance['subject_name'] = cur_dictionary_name
+                    combined_dictionary['words'][cur_word].append(cur_word_instance)
+
+            for cur_letter in cur_dictionary_data['letters']:
+                if not cur_letter in combined_dictionary:
+                    combined_dictionary['letters'][cur_letter] = []
+
+                for cur_letter_instance in cur_dictionary_data['letters'][cur_letter]:
+                    cur_letter_instance['subject_name'] = cur_dictionary_name
+                    combined_dictionary['letters'][cur_letter].append(cur_letter_instance)
+
+                    sum_of_letter_widths += cur_letter_instance['right'] - cur_letter_instance['left'] 
+                    num_of_letter_widths += 1
+
+    return (combined_dictionary, sum_of_letter_widths // num_of_letter_widths)
+
+
+if __name__ == '__main__':
+    create_titlecut(target_text='though~ rely on bad ligma!', dictionaries=['azurlane']).save('output.png')
